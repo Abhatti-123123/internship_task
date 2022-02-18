77fea55573ac84569fef936c631080e90e606620
Abhatti-123123 <64663617+Abhatti-123123@users.noreply.github.com>
2022-02-18 22:47:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+import './file1.css';
+import './App.css';
+import './ui.css';
+import './ui2.css';
+import './fonts.css';
+import './fontttt.css';
+import './animation.css';
+// import './dialog.css';
+import './elementor.css';
+import './font1.css';
+import './font2.css';
+import './ant.css';
+import './font3.css';
+import './font4.css';
+import './font5.css';
+import './font_main.css';
+import Home from './home/home';
+function App() {
+  return (
+    <div id="body-color">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
